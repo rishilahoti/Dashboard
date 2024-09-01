bff74e84e8605dca4d9f1a865ee0d7b2006a2d6b
rishilahoti <rishilahoti99@gmail.com>
2024-09-01 13:07:44 +0530

    html icon added

--- src/app/components/Course.tsx
+++ src/app/components/Course.tsx
@@ -1,22 +1,26 @@
 import CourseUpdateButton from "./CourseUpdateButton";
+import { TiHtml5 } from "react-icons/ti";
 
 export default function Course() {
-    return(
-        <section className="flex w-full min-h-[90px] justify-center items-center lg:gap-10 gap-3 rounded-xl border border-gray-200 p-2 lg:p-4">
-            <div className="flex w-fit justify-center items-center ">
-                HTML
-            </div>
-            <div className="flex w-full flex-col gap-1">
-                <h2 className="md:text-[18px] text-[12px] font-semibold">
-                    Hyper Text Markup Language
-                </h2>
-                <div className="md:text-[10px] text-[8px] font-semibold flex">
-                    <p className="text-slate-500">Question: 08 | Duration: 15 mins | Submitted on 1 Sept 2024</p>
-                </div>
-            </div>
-            <div className="">
-                <CourseUpdateButton/>
-            </div>
-        </section>
-    )
+	return (
+		<section className="flex w-full min-h-[90px] justify-center items-center lg:gap-10 gap-3 rounded-xl border border-gray-200 p-2 lg:p-4">
+			<div className="flex w-fit justify-center items-center ">
+				<TiHtml5 size={42} color="red" />
+			</div>
+			<div className="flex w-full flex-col gap-1">
+				<h2 className="md:text-[18px] text-[12px] font-semibold">
+					Hyper Text Markup Language
+				</h2>
+				<div className="md:text-[10px] text-[8px] font-semibold flex">
+					<p className="text-slate-500">
+						Question: 08 | Duration: 15 mins | Submitted on 1 Sept
+						2024
+					</p>
+				</div>
+			</div>
+			<div className="">
+				<CourseUpdateButton />
+			</div>
+		</section>
+	);
 }
